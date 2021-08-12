b798522b2c6c391efb2f82d62d53bae45ab3e8b4
Vivek Hutheesing <vhutheesing@comcast.net>
2021-08-11 23:27:26 -0700

    Update layout.js

--- src/components/layout.js
+++ src/components/layout.js
@@ -36,16 +36,9 @@ const Layout = ({ children }) => {
         <main>{children}</main>
         <footer>
             <div className={`pgFoo`}> 
-                <div className={`fooLft`}> 
-                  <a href="aboutus.html" target="_blank">About Us</a>
-                  <a href="/benefits.html" target="_blank">Benefits</a>
-                  <a href="/blog.html" target="_blank">Blog</a>
-                  <a href="/FAQ.html" target="_blank">FAQ</a>
-                  <a href="/privacypolicy.html" target="_blank">Privacy Policy</a>
-                  <a href="/siteterms.html" target="_blank">Site Terms</a> 
-              </div> 
-               <div className={`fooRht`}> Copyright © 2012 rBlock Inc. All rights reserved. </div> <div className={`clr`}></div> 
-              </div>
+
+               <div style={{textAlign:'center'}}> Copyright © 2021 rBlock Inc. All rights reserved. </div> <div className={`clr`}></div> 
+            </div>
 
         </footer>
       </div>
