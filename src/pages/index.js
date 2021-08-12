270f8eb2a5309e31e3f948b78d4fe59bafa1ca95
Vivek Hutheesing <vhutheesing@comcast.net>
2021-08-11 21:23:47 -0700

    Head

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,10 +7,10 @@ import Seo from "../components/seo"
 
 const IndexPage = () => (
   <Layout>
-    <Seo title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
+    <Seo title="rBlock" />
+    <div style={{background:#f5f5f5;height:110px;}}>
+      
+    </div>
     <StaticImage
       src="../images/gatsby-astronaut.png"
       width={300}
