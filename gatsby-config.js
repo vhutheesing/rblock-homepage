5210ba890aeb45b3f7b190433d48fe1a398999b6
Vivek Hutheesing <vhutheesing@comcast.net>
2021-08-11 21:38:11 -0700

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,9 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
+    title: `Connects Communities`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
     author: `@gatsbyjs`,
-    siteUrl: `https://gatsbystarterdefaultsource.gatsbyjs.io/`,
+    siteUrl: `https://rblock.com/`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
