a05128409d57a0b02ed1dbbefd909c4776e31806
Vivek Hutheesing <vhutheesing@comcast.net>
2021-08-11 23:28:39 -0700

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,13 +10,16 @@ const IndexPage = () => (
   <Layout>
     <Seo title="rBlock" />
 
-    <div  style={{textAlign:`center`,verticalAlign:`middle`, height:`500px`, marginBottom:`20px`}}>
+    <div  style={{textAlign:`center`,verticalAlign:`middle`, marginBottom:`20px`}}>
     <StaticImage
       src="../images/videographic.png"
       quality={95}
       formats={["AUTO", "WEBP", "AVIF"]}
       alt="Video"
     />
+        
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/7osa7cmI2MY?rel=0" frameborder="0" allowfullscreen=""></iframe>
+
     </div>
      <div style={{textAlign:`center`, margin:`10px`}}>rBlock is a private, location-based communication network for residents.
 Its mission is to enable greater reach for the communications you send and greater relevance for the ones you receive.</div>
