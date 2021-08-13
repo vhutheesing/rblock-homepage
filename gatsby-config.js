5ed8a0bf0bf2c9dc2a86a717a860c903ce82759c
Vivek Hutheesing <vhutheesing@comcast.net>
2021-08-12 20:21:26 -0700

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -26,7 +26,7 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/fav-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/rblock_logo_225x75.png`, // This path is relative to the root of the site.
       },
     },
     `gatsby-plugin-gatsby-cloud`,
